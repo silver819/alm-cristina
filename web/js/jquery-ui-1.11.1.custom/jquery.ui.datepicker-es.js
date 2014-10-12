61a1c2814da94d1ff3ca384f0dfbf3f1d0381626
silver819 <silver819@gmail.com>
2014-10-12 10:21:40 +0200

    localize datepicker

--- web/js/jquery-ui-1.11.1.custom/jquery.ui.datepicker-es.js
+++ web/js/jquery-ui-1.11.1.custom/jquery.ui.datepicker-es.js
@@ -1,6 +1,17 @@
-$.datepicker.regional["es"]({
-	dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
-	dayNames: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
-	monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
-	monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-});
+$.datepicker.regional['es'] = {
+	closeText: 			'Cerrar',
+	prevText: 			'<Ant',
+	nextText: 			'Sig>',
+	currentText: 		'Hoy',
+	monthNames: 		['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+	monthNamesShort: 	['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+	dayNames: 			['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+	dayNamesShort: 		['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+	dayNamesMin: 		['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+	weekHeader: 		'Sm',
+	dateFormat: 		'dd/mm/yy',
+	firstDay: 			1,
+	isRTL: 				false,
+	showMonthAfterYear: false,
+	yearSuffix: 		''
+};
