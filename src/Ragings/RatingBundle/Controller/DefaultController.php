a693581061f5fd70ad3e00cc6c7a492689190876
silver819 <silver819@gmail.com>
2015-09-13 12:44:36 +0200

    estadisticas

--- src/Ragings/RatingBundle/Controller/DefaultController.php
+++ src/Ragings/RatingBundle/Controller/DefaultController.php
@@ -117,12 +117,12 @@ class DefaultController extends Controller
         $xAxisHourlyRated = $xAxisDailyRated = array();
         $yAxisHourlyRated = $yAxisDailyRated = array();
         foreach($top10clients['hour'] as $top10hourly){
-            $xAxisHourlyRated[] = $top10hourly['name'] . ' ' . $top10hourly['surname'] . '(' . $top10hourly['email'] . ')';
-            $yAxisHourlyRated[] = array($top10hourly['name'] . ' ' . $top10hourly['surname'] . '(' . $top10hourly['email'] . ')', $top10hourly['numBookings']);
+            $xAxisHourlyRated[] = $top10hourly['name'] . ' ' . $top10hourly['surname'] . ' (' . $top10hourly['email'] . ')';
+            $yAxisHourlyRated[] = array($top10hourly['name'] . ' ' . $top10hourly['surname'] . ' (' . $top10hourly['email'] . ')', $top10hourly['numBookings']);
         }
         foreach($top10clients['day'] as $top10daily){
-            $xAxisDailyRated[] = $top10daily['name'] . ' ' . $top10daily['surname'] . '(' . $top10daily['email'] . ')';
-            $yAxisDailyRated[] = array($top10daily['name'] . ' ' . $top10daily['surname'] . '(' . $top10daily['email'] . ')', $top10daily['numBookings']);
+            $xAxisDailyRated[] = $top10daily['name'] . ' ' . $top10daily['surname'] . ' (' . $top10daily['email'] . ')';
+            $yAxisDailyRated[] = array($top10daily['name'] . ' ' . $top10daily['surname'] . ' (' . $top10daily['email'] . ')', $top10daily['numBookings']);
         }
 
         $chartTop10hourlyClients = new Highchart();
