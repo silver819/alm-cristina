07092691821bfb42747bc1076feaec7586ca9880
silver819 <silver819@gmail.com>
2015-09-13 11:54:54 +0200

    estadisticas

--- src/Ragings/RatingBundle/Controller/DefaultController.php
+++ src/Ragings/RatingBundle/Controller/DefaultController.php
@@ -55,7 +55,7 @@ class DefaultController extends Controller
         //ldd($top5bookings);
 
         // Chart
-        $pieOptions = array(
+        $chartOptions = array(
             'allowPointSelect'  => true,
             'cursor'            => 'pointer',
             'dataLabels'        => array('enabled' => false),
@@ -65,31 +65,51 @@ class DefaultController extends Controller
         $chartTop5hourlyBooked = new Highchart();
         $chartTop5hourlyBooked->chart->renderTo('top5hourlyBookingsProperties');
         $chartTop5hourlyBooked->title->text('Por hora');
-        $chartTop5hourlyBooked->plotOptions->pie($pieOptions);
+        $chartTop5hourlyBooked->plotOptions->pie($chartOptions);
 
         $chartTop5dailyBooked = new Highchart();
         $chartTop5dailyBooked->chart->renderTo('top5dailyBookingsProperties');
         $chartTop5dailyBooked->title->text('Por día');
-        $chartTop5dailyBooked->plotOptions->pie($pieOptions);
+        $chartTop5dailyBooked->plotOptions->pie($chartOptions);
 
         $data1 = array();
-        foreach($top5bookings['hour'] as $property){
-            $data1[] = array(
-                $property['propertyName'] . ', ' . $property['ownerName'] . ' ' . $property['ownerSurname'] . ' (' . $property['ownerEmail'] . ' )',
-                (int)$property['numBookings']);
-        }
-
         $data2 = array();
-        foreach($top5bookings['day'] as $property){
-            $data2[] = array(
-                $property['propertyName'] . ', ' . $property['ownerName'] . ' ' . $property['ownerSurname'] . ' (' . $property['ownerEmail'] . ' )',
-                (int)$property['numBookings']);
-        }
+        foreach($top5bookings['hour'] as $property) $data1[] = array($property['propertyName'] . ', ' . $property['ownerName'] . ' ' . $property['ownerSurname'] . ' (' . $property['ownerEmail'] . ' )', (int)$property['numBookings']);
+        foreach($top5bookings['day'] as $property)  $data2[] = array($property['propertyName'] . ', ' . $property['ownerName'] . ' ' . $property['ownerSurname'] . ' (' . $property['ownerEmail'] . ' )', (int)$property['numBookings']);
+
 
         $chartTop5hourlyBooked->series(array(array('type' => 'pie','name' => 'Reservas', 'data' => $data1)));
         $chartTop5dailyBooked->series(array(array('type' => 'pie','name' => 'Reservas', 'data' => $data2)));
 
         ## Alojamientos mejor valorados (por día y por hora)
+        $top5ratings = $this->getTop5Ratings();
+
+        $xAxisHourlyRated = $xAxisDailyRated = array();
+        $yAxisHourlyRated = $yAxisDailyRated = array();
+        foreach($top5ratings['hour'] as $rating){
+            $xAxisHourlyRated[] = $rating['propertyName'];
+            $yAxisHourlyRated[] = array($rating['propertyName'], $rating['meanRating']);
+        }
+        foreach($top5ratings['day'] as $rating){
+            $xAxisDailyRated[] = $rating['propertyName'];
+            $yAxisDailyRated[] = array($rating['propertyName'], $rating['meanRating']);
+        }
+
+        $chartTop5hourlyRated = new Highchart();
+        $chartTop5hourlyRated->chart->renderTo('top5hourlyRatedProperties');
+        $chartTop5hourlyRated->chart->type('column');
+        $chartTop5hourlyRated->title->text('Resultados de la encuesta sobre 5 puntos');
+        $chartTop5hourlyRated->plotOptions->pie($chartOptions);
+        $chartTop5hourlyRated->xAxis->categories($xAxisHourlyRated);
+        $chartTop5hourlyRated->series(array(array('type' => 'column', 'name' => 'Valoración media', 'data' => $yAxisHourlyRated)));
+
+        $chartTop5dailyRated = new Highchart();
+        $chartTop5dailyRated->chart->renderTo('top5dailyRatedProperties');
+        $chartTop5dailyRated->chart->type('column');
+        $chartTop5dailyRated->title->text('Resultados de la encuesta sobre 5 puntos');
+        $chartTop5dailyRated->plotOptions->pie($chartOptions);
+        $chartTop5dailyRated->xAxis->categories($xAxisDailyRated);
+        $chartTop5dailyRated->series(array(array('type' => 'column', 'name' => 'Valoración media', 'data' => $yAxisDailyRated)));
 
         ## Clientes más activos
 
@@ -98,7 +118,9 @@ class DefaultController extends Controller
             'RagingsRatingBundle:Statistics:index.html.twig',
             array(
                 'chartTop5hourlyBooked' => $chartTop5hourlyBooked,
-                'chartTop5dailyBooked'  => $chartTop5dailyBooked
+                'chartTop5dailyBooked'  => $chartTop5dailyBooked,
+                'chartTop5hourlyRated'  => $chartTop5hourlyRated,
+                'chartTop5dailyRated'   => $chartTop5dailyRated
             ));
 
     }
@@ -138,6 +160,50 @@ class DefaultController extends Controller
                     $aux['ownerSurname']    = $ownerData->getSurname();
 
                     $arrayReturn[$one['typeRent']][] = $aux;
+                }
+            }
+        }
+
+        //ldd($arrayReturn);
+
+        return $arrayReturn;
+    }
+
+    public function getTop5Ratings(){
+
+        $arrayReturn = array();
+
+        $results = $this->getDoctrine()
+                ->getManager()
+                ->createQuery('SELECT count(r.id) as numRatings, SUM(r.ubicacion) as ubicacionCount,
+                               SUM(r.llegar) as llegarCount, SUM(r.limpieza) as limpiezaCount,
+                               SUM(r.material) as materialCount, SUM(r.caracteristicas) as caracteristicasCount,
+                               SUM(r.gestiones) as gestionesCount, SUM(r.usabilidad) as usabilidadCount,
+                               b.activityID
+                               FROM RagingsRatingBundle:Rating r
+                               INNER JOIN BookingsBookingBundle:Booking b
+                               WHERE b.id = r.reservationNumber
+                               GROUP BY b.activityID')
+                ->getResult();
+
+        //ldd($results);
+
+        if(!empty($results)){
+            foreach($results as $one){
+
+                $property = $this->getDoctrine()
+                    ->getRepository('ReservableActivityBundle:Activity')
+                    ->findByPropertyID($one['activityID']);
+
+                if(!isset($arrayReturn[$property->getTypeRent()]) || count($arrayReturn[$property->getTypeRent()]) <= 5) {
+
+                    $aux = array();
+
+                    $aux['propertyName']    = $property->getName();
+                    $aux['numRatings']      = $one['numRatings'];
+                    $aux['meanRating']      = ($one['ubicacionCount'] + $one['llegarCount'] + $one['limpiezaCount'] + $one['materialCount'] + $one['caracteristicasCount'] + $one['gestionesCount'] + $one['usabilidadCount']) / (7 * $one['numRatings']);
+
+                    $arrayReturn[$property->getTypeRent()][] = $aux;
                 }
             }
         }
