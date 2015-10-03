9c192c6fbd42b94bd95959186ad193cd47403755
silver819 <silver819@gmail.com>
2015-10-03 12:49:36 +0200

    menu

--- src/Admin/AdminBundle/EventListener/SidebarNavigation.php
+++ src/Admin/AdminBundle/EventListener/SidebarNavigation.php
@@ -29,21 +29,30 @@ class SidebarNavigation
     {
         $menu      = array();
         $rootItems = array();
+        if(!$this->context->isGranted('ROLE_SUPER_ADMIN')) {
+            $rootItems[] = $viewUsers      = new MenuItemModel('fos_user_profile_show',        'Mi perfil',             'fos_user_profile_show',       $menu, 'glyphicon glyphicon-user');
+        }
         if($this->context->isGranted('ROLE_SUPER_ADMIN')){
             $rootItems[] = $viewUsers      = new MenuItemModel('view-users',        'Ver usuarios',             'view_users',       $menu, 'glyphicon glyphicon-eye-open');
         }
 
-        $rootItems[] = $viewProperties     = new MenuItemModel('view-activities',  'Ver propiedades',          'view_activities',  $menu, 'glyphicon glyphicon-eye-open');
+        if($this->context->isGranted('ROLE_ADMIN')) {
+            $rootItems[] = $viewProperties = new MenuItemModel('view-activities', 'Ver propiedades', 'view_activities', $menu, 'glyphicon glyphicon-eye-open');
+        }
         $rootItems[] = $bookings           = new MenuItemModel('bookings',         'Reservas',                 'consultBookings',  $menu, 'glyphicon glyphicon-check');
-        $rootItems[] = $calendar           = new MenuItemModel('calendarBookings', 'Calendario',               'calendarBookings', $menu, 'fa-calendar');
-        $rootItems[] = $history            = new MenuItemModel('historyBookings',  'Historial de reservas',    'historyBookings',  $menu, 'fa fa-th');
+        if($this->context->isGranted('ROLE_ADMIN')) {
+            $rootItems[] = $calendar = new MenuItemModel('calendarBookings', 'Calendario', 'calendarBookings', $menu, 'fa-calendar');
+            $rootItems[] = $history = new MenuItemModel('historyBookings', 'Historial de reservas', 'historyBookings', $menu, 'fa fa-th');
+        }
         $rootItems[] = $addProperty        = new MenuItemModel('add-property',     'Nueva propiedad',           'new_activity',    $menu, 'glyphicon glyphicon-plus');
         if($this->context->isGranted('ROLE_SUPER_ADMIN')){
             $rootItems[] = $viewOwners      = new MenuItemModel('admin-types',     'Gestionar tipos',          'admin_types',      $menu, 'fa fa-text-width');
             $rootItems[] = $viewOwners      = new MenuItemModel('admin-features',  'Gestionar características','admin_features',   $menu, 'fa fa-th-list');
             $rootItems[] = $viewOwners      = new MenuItemModel('types-features',  'Tipos y características',  'new_features',     $menu, 'fa fa-exchange');
         }
-        $rootItems[] = $statistics         = new MenuItemModel('statistics',       'Estadísticas',             'statistics',       $menu, 'fa fa-bar-chart');
+        if($this->context->isGranted('ROLE_ADMIN')) {
+            $rootItems[] = $statistics = new MenuItemModel('statistics', 'Estadísticas', 'statistics', $menu, 'fa fa-bar-chart');
+        }
 
         //$statistics->addChild(new MenuItemModel('ui-elements-general', 'General', 'avanzu_admin_ui_gen_demo', $earg))
         //           ->addChild($icons = new MenuItemModel('ui-elements-icons', 'Icons', 'avanzu_admin_ui_icon_demo', $earg));
